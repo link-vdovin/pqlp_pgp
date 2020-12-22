b6b74eb6036982554e8f69378a0eaa28c7459bf9
Vasiliy Vdovin <vasiliy@vdovin.link>
2020-12-22 16:05:55 +0100

    Other

--- utilities/images_name_generator.php
+++ utilities/images_name_generator.php
@@ -4,9 +4,9 @@ $x = 0;
 
 while($x <= 60) {
     echo '<picture>';
-    echo '<source media="(max-height:640px)" srcset="/assets/images/3d/245×320/pgp_'. $x .'.jpg">';
-    echo '<source media="(max-height:1280px)" srcset="/assets/images/3d/490×640/pgp_'. $x .'.jpg">';
-    echo '<img src="/assets/images/3d/980×1280/pgp_'. $x .'.jpg">';
+    echo '<source media="(max-height:1080px)" srcset="/assets/images/3d/middle/pgp_'. $x .'.jpg">';
+    echo '<source media="(max-height:760px)" srcset="/assets/images/3d/small/pgp_'. $x .'.jpg">';
+    echo '<img src="/assets/images/3d/big/pgp_'. $x .'.jpg">';
     echo '</picture>';
     $x++;
 }
